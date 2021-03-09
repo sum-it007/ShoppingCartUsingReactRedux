fa3546eafec16ea7651b6066299d2904b9ae25ef
sum-it007 <sumitkumar5631@gmail.com>
2021-03-09 18:00:14 +0530

    second commit

--- src/Cart.js
+++ src/Cart.js
@@ -1,21 +1,24 @@
 import React from 'react'
-import {FlatList, Text,TouchableOpacity,View} from 'react-native'
+import {FlatList, Text,TouchableOpacity,View,StyleSheet} from 'react-native'
 import { connect } from 'react-redux'
 
 const Cart = (props) => {
     return ( 
         <View>
-        <Text>{props.user}'s Cart</Text>
-        <Text>Total Items in the Cart : {props.totalItems}</Text>
-        <Text>Items in the cart are : </Text>
-        <Text>Total Price : {props.totalPrice}</Text>
-        
+        <View style={styles.cartInfo}>
+        <Text style={styles.username}> {props.user}'s Cart</Text>
+        <Text style={styles.cartInfoText}>Total Items in the Cart : {props.totalItems}</Text>
+        <Text style={styles.cartInfoText}>Total Price : {props.totalPrice}</Text>
+        <Text style={styles.cartInfoText3}>Items in the cart are : </Text>
+        </View>
         <FlatList
         data={props.items}
         keyExtractor={(item,index)=>index}
         renderItem={({item})=>{
             return(
+                <View style={styles.listItem}>
                 <Text>{item}</Text>
+                </View>
             )
         }}
         />
@@ -31,3 +34,24 @@ const mapStateToProps = (state) => ({
 })
 
 export default connect(mapStateToProps)(Cart)
+
+const styles = StyleSheet.create({
+    listItem:{
+        margin:10,
+        marginHorizontal:20,
+    },
+    username:{
+        fontSize:25,
+        fontWeight:'bold'
+    },
+    cartInfo:{
+        margin:15,
+    },
+    cartInfoText:{
+        fontSize:18
+    },
+    cartInfoText3:{
+        fontSize:18,
+        marginTop:30
+    }
+})
